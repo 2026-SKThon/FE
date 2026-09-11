227ec78f4bc1054388497d424e7aa7410d19ee34
dbszl3 <yoonji0811@naver.com>
2026-09-12 03:30:51 +0900

    🔧 Settings: API 공통 설정 수정

--- src/api/api.js
+++ src/api/api.js
@@ -11,20 +11,4 @@ const api = axios.create({
   },
 });
 
-// { success, code, message, data } 형태일 때만 data를 꺼낸다
-function isEnvelope(body) {
-  return (
-    body !== null &&
-    typeof body === "object" &&
-    "success" in body &&
-    "data" in body
-  );
-}
-
-api.interceptors.response.use((response) =>
-  isEnvelope(response.data)
-    ? { ...response, data: response.data.data }
-    : response,
-);
-
 export default api;

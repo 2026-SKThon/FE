a302d45bd0b288cb2a0887cb86439e23257d3322
hxx-dev <hxxchangdev@gmail.com>
2026-09-11 20:52:33 +0900

    🎨 Design: 체온 입력 필드 컴포넌트 구현

--- src/components/home/record/form/CornerIllustration.jsx
+++ src/components/home/record/form/CornerIllustration.jsx
@@ -1,10 +1,10 @@
-export default function CornerIllustration({ src, size, left, top }) {
+export default function CornerIllustration({ src, width, height, left, top }) {
   return (
     <img
       src={src}
       alt=""
       className="pointer-events-none absolute"
-      style={{ width: size, height: size, left, top }}
+      style={{ width, height, left, top }}
     />
   );
 }

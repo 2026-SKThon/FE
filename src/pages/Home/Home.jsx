dd2e8c0aff806785c2b1728551f26a4913ee221c
hxx-dev <hxxchangdev@gmail.com>
2026-09-11 23:08:41 +0900

    🎨 Design: 홈 하단 블록 레이아웃 개편

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import FeverStatusCard from "../../components/home/FeverStatusCard";
 import HomeTopBar from "../../components/home/HomeTopBar";
 import QuickActions from "../../components/home/QuickActions";
+import StatusNoticeCard from "../../components/home/StatusNoticeCard";
 import TrendCard from "../../components/home/TrendCard";
 import { childStatus } from "../../constants/childStatus";
 import { FEVER_LEVEL } from "../../constants/fever";
@@ -11,30 +12,33 @@ import { resolveFeverLevel } from "../../utils/fever";
 
 export default function Home() {
   const navigate = useNavigate();
-  const level = FEVER_LEVEL[resolveFeverLevel(childStatus)];
+  const levelKey = resolveFeverLevel(childStatus);
+  const level = FEVER_LEVEL[levelKey];
 
   return (
     <div className="flex flex-1 flex-col gap-[12px] bg-[#FBFBFB] px-[20px] pb-[117px]">
-      <HomeTopBar deviceConnected={childStatus.deviceConnected} />
+      <HomeTopBar tag={level.tag} />
       <FeverStatusCard
         childName={childStatus.childName}
         temperature={childStatus.currentTemperature}
-        temperatureColor={level.temperatureColor}
-        showChildLabel={level.showChildLabel}
-        statusMessage={level.pill.message}
         caption={childStatus.caption}
+        levelKey={levelKey}
+        level={level}
       />
-      <TrendCard
-        title="최근 6시간"
-        trend={recentTemperatureTrend}
-        color={level.chartColor}
-        showEndDot={level.showEndDot}
-        onViewRecords={() => navigate("/records")}
-      />
-      <QuickActions
-        recordLabel={level.primaryAction}
-        hospitalLabel={level.secondaryAction}
-      />
+      <div className="flex flex-col gap-[16px]">
+        <TrendCard
+          title="최근 6시간"
+          trend={recentTemperatureTrend}
+          color={level.chartColor}
+          showEndDot={level.showEndDot}
+          onViewRecords={() => navigate("/records")}
+        />
+        <StatusNoticeCard {...level.notice} />
+        <QuickActions
+          recordLabel={level.primaryAction}
+          hospitalLabel={level.secondaryAction}
+        />
+      </div>
     </div>
   );
 }

f4079aa673cd7192765f215aa0bc3694cf424001
dbszl3 <yoonji0811@naver.com>
2026-09-11 12:44:28 +0900

    ♻️ Refactor: 공통 레이아웃에 하단 네비게이션 적용

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 
+import Footer from "../components/footer/footer";
+
 export default function RootLayout() {
   return (
-    <div className="flex justify-center">
-      <div className="w-[393px] h-[852px] bg-[#FBFBFB]">
+    <div className="flex min-h-screen justify-center bg-[#F7F7F7]">
+      <div className="flex min-h-screen w-full max-w-[393px] flex-col bg-white">
         <Outlet />
+        <Footer />
       </div>
     </div>
   );

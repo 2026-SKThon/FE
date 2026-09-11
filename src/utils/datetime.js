cf6f041ccb366526b7ed56704552d2914bf6a9fe
hxx-dev <hxxchangdev@gmail.com>
2026-09-11 20:52:20 +0900

    ✨ Feat: 측정 일시 표기 포맷 유틸 추가

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -26,3 +26,13 @@ export function formatDateLabel(isoString) {
 
   return `${date.getMonth() + 1}월 ${date.getDate()}일 ${WEEKDAYS[date.getDay()]}요일`;
 }
+
+export function formatDateTimeLabel(isoString) {
+  const date = new Date(isoString);
+  const hours = date.getHours();
+  const meridiem = hours < 12 ? "오전" : "오후";
+  const hour12 = hours % 12 === 0 ? 12 : hours % 12;
+  const minute = String(date.getMinutes()).padStart(2, "0");
+
+  return `${date.getMonth() + 1}월 ${date.getDate()}일 ${meridiem} ${hour12}:${minute}`;
+}

5504795cef8c81d09ceef21ef8b1b8196d97429f
hxx-dev <hxxchangdev@gmail.com>
2026-09-11 15:42:58 +0900

    🎨 Design: 기록 목록 UI 구현

---
+++ src/components/home/record/RecordList.jsx
@@ -0,0 +1,22 @@
+import RecordItem from "./RecordItem";
+import { formatRecord } from "../../../utils/record";
+
+export default function RecordList({ records }) {
+  return (
+    <div className="flex flex-col gap-[8px]">
+      {records.map((record) => {
+        const { time, title, description, type } = formatRecord(record);
+
+        return (
+          <RecordItem
+            key={record.recordId}
+            time={time}
+            title={title}
+            description={description}
+            type={type}
+          />
+        );
+      })}
+    </div>
+  );
+}

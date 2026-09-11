d563811d05ed0198c21dcb665bfc111f9c3d669c
hxx-dev <hxxchangdev@gmail.com>
2026-09-12 03:14:08 +0900

    ♻️ Refactor: axios 인스턴스 일원화 및 응답 인터셉터 추가

--- src/api/hospitals.js
+++ src/api/hospitals.js
@@ -1,9 +1,9 @@
-import { apiClient } from "./client";
+import api from "./api";
 
 // GET /api/hospitals/search - 병원명 또는 주소 키워드로 병원 검색
 export async function searchHospitals({ keyword, latitude, longitude }) {
-  const { data } = await apiClient.get("/api/hospitals/search", {
+  const { data } = await api.get("/api/hospitals/search", {
     params: { keyword, latitude, longitude },
   });
-  return data.data.hospitals;
+  return data.hospitals;
 }

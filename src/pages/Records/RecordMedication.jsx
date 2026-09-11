a76f465da2db355fe356de0ce7f98c36b9a08816
hxx-dev <hxxchangdev@gmail.com>
2026-09-11 20:34:48 +0900

    ✨ Feat: 기록 추가 하위 화면 라우팅 연결

---
+++ src/pages/Records/RecordMedication.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/header/header";
+
+export default function RecordMedication() {
+  return (
+    <div className="flex flex-1 flex-col bg-[#FBFBFB]">
+      <Header title="복약 기록" />
+      <div className="flex flex-1 flex-col gap-[14px] px-[20px] pt-[20px] pb-[117px]" />
+    </div>
+  );
+}
